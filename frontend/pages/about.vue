ce6e8ad00ee210d87c1a1610091e874293da0dba
DoosicEE <strou73@naver.com>
2022-11-22 22:47:53 +0900

    Initial commit (defaultInit)

---
+++ frontend/pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+            Aenean commodo ligula eget dolor. Aenean massa. 
+            Cum sociis natoque penatibus et magnis dis parturient montes, 
+            nascetur ridiculus mus. Donec quam felis</p>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+            Aenean commodo ligula eget dolor. Aenean massa. 
+            Cum sociis natoque penatibus et magnis dis parturient montes, 
+            nascetur ridiculus mus. Donec quam felis</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+    h2{
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p{
+        margin: 20px 0;
+    }
+</style>
