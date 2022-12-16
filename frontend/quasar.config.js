c988872b2781b97536dbe865e503abdc22217c06
DoosicEE <strou73@naver.com>
2022-12-16 10:14:15 +0900

    #Update Quasar Component
    얼럿창, 로딩바 등... 정말 많은걸 지원해줘서 좋다

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -27,7 +27,12 @@ module.exports = configure(function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli/boot-files
-    boot: ['initialization', 'constants'],
+    boot: [
+      'initialization',
+      'constants',
+      'loading-plugin',
+      'loading-bar-plugin',
+    ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: ['app.scss'],
@@ -86,6 +91,8 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
       cssAddon: true,
+      // 간단하게 설정할때 config 사용하면 좋다.
+      // 플래그를 새워야 할때는 boot파일을 적극 이용하는게 좋다
       config: {
         screen: {
           bodyClasses: true,
@@ -103,7 +110,7 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: ['LocalStorage'],
+      plugins: ['LocalStorage', 'Loading', 'LoadingBar'],
     },
 
     // animations: 'all', // --- includes all animations
--- frontend/src/pages/FormHandling.vue
+++ frontend/src/pages/FormHandling.vue
@@ -82,7 +82,9 @@
 
 <script setup>
 import { ref } from 'vue';
+import { useQuasar } from 'quasar';
 
+const $q = useQuasar();
 const myForm = ref(null);
 const form = ref({
   title: '',
@@ -114,9 +116,13 @@ const reset = () => {
 const onSubmit = () => {
   if (form.value.accept !== true) {
     alert('동의 해주세요');
-  } else {
-    alert('성공~!');
+    return;
   }
+  $q.loading.show();
+  setTimeout(() => {
+    $q.loading.hide();
+    alert('성공~!');
+  }, 3000);
 };
 const onReset = () => {
   form.value.title = '';
